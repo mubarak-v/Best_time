91820c55dd48d6ab184bc80a5d3fae04460c18e7
husni mubarak <husnimubarakvattoli1@gmail.com>
2024-11-13 11:32:58 +0530

    feat:completed seller dashboard

--- product/urls.py
+++ product/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
   
     path('', views.products, name = "products"),
     path('<int:pk>/', views.product_details, name = "products_details"),
-    path('dashboard/', views.product_dashboard, name = "dashboard"),
-    path('dashboard/edit/<int:product_id>/', views.edit_product, name="edit_product"),
-    path('dashboard/add/', views.add_product, name="add_product"),
     path('cart/', views.cart_view, name="cart_view"),
     path('cart/add/<int:product_id>/<str:action>/', views.add_cart, name='add_cart'),
     path('cart/remove/<int:product_id>', views.delete_cart_product , name = 'delete_cart_product'),
